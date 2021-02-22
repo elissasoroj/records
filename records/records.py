c448571b1d21c0baa4f4a38bd23a2b58d10b6bc0
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-02-22 14:05:22 -0500

    updated code

--- records/records.py
+++ records/records.py
@@ -8,17 +8,21 @@ import requests
 import pandas as pd
 
 class Records:
+    """
+    Uses genusKey to query GBIF API and return data as JSON or pandas dataframe
+    """
     def __init__(self, year, genusKey):
-        
+
         # store input params
         self.year = year
         self.genusKey = genusKey
-        
+
         # will be used to store output results
         self.df = pd.DataFrame()
         self.json = {}
-        
+
     def generate_genus_key(self):
+        "allows user to query using genus string"
 
         if type(self.genusKey) is int:
             self.genusKey = genusKey
@@ -40,10 +44,10 @@ class Records:
 
     def get_single_batch(self, offset=0, limit=20):
         "returns JSON result for a small batch query"
-        # ...
+        
         self.offset = offset
         self.limit = limit
-        
+
         batch = requests.get(
             url="https://api.gbif.org/v1/occurrence/search/",
             params={
@@ -54,18 +58,33 @@ class Records:
             }
         )
         return batch.json()
-        
+     
     def get_all_records(self):
         "stores result for all records to self.json and self.df"
-        # ...
-        res = requests.get(
-            url="https://api.gbif.org/v1/occurrence/search/",
-            params={
-                "key": self.genusKey,
-                "year": self.year,
-            }
-        )
-        
-        self.json = res.json()
+        # for storing results
+        storeres = []
+        # continue until we call 'break'
+        offset = 0
+        while 1:
+            # get JSON data for a batch 
+            jdata = self.get_single_batch(offset, 300)
+
+            # increment counter by 300 (the max limit)
+            offset += 300
+
+            # add this batch of data to the growing list
+            storeres.extend(jdata["results"])
+
+            # stop at the end of records
+            if jdata["endOfRecords"]:
+                print(f'Done. Found {len(storeres)} records')
+                break
+ 
+            # print a dot on each rep to show progress
+            print('.', end='')
+
+        #store the results as json and pandas dataframe
+        self.json = storeres
         self.df = pd.json_normalize(self.json["results"])
+
         
